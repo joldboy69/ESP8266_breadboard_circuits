a8c3339500f810d7e59e4880f77804801c21ee2b
joldboy69 <52323012+joldboy69@users.noreply.github.com>
2020-04-21 17:02:15 -0400

    Add files via upload

---
+++ sourceCode/beginnerProject/project1.ino
@@ -0,0 +1,14 @@
+int a = 13; // sets variable "a" equal to pin 13 on our board
+
+void setup() { // setup code to run only once
+  
+  pinMode(a, OUTPUT); // sets the board to utilize pin 13 (which we set as "a" above) as the output
+
+void loop() { // creates a loop
+  
+
+  digitalWrite(a, HIGH); // turns the pin charge on 
+
+  
+
+}
